87847cc78b88ed9aea2c7d24d3b060ee7f6a4995
Hau Pham <hauph@hotmail.com>
2021-12-03 08:45:14 +0700

    WIP: native device features

--- src/scripts/pwa/feed.js
+++ src/scripts/pwa/feed.js
@@ -7,6 +7,11 @@ const sharedMomentsArea = document.querySelector('#shared-moments');
 const form = document.querySelector('form');
 const titleInput = document.querySelector('#title');
 const locationInput = document.querySelector('#location');
+const videoPlayer = document.querySelector('#player');
+const canvasElement = document.querySelector('#canvas');
+const captureButton = document.querySelector('#capture-btn');
+const imagePicker = document.querySelector('#image-picker');
+const imagePickerArea = document.querySelector('#pick-image');
 
 // Currently not in use, allows to save assets in cache on demand otherwise
 function onSaveButtonClicked(event) {
@@ -54,6 +59,23 @@ function createCard(data) {
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
+captureButton.addEventListener('click', (event) => {
+  canvasElement.style.display = 'block';
+  videoPlayer.style.display = 'none';
+  captureButton.style.display = 'none';
+  const context = canvasElement.getContext('2d');
+  context.drawImage(
+    videoPlayer,
+    0,
+    0,
+    canvasElement.width,
+    videoPlayer.videoHeight / (videoPlayer.videoWidth / canvasElement.width),
+  );
+  videoPlayer.srcObject.getVideoTracks().forEach((track) => {
+    track.stop();
+  });
+});
+
 function updateUI(data) {
   clearCards();
   for (let i = 0; i < data.length; i++) {
@@ -61,8 +83,40 @@ function updateUI(data) {
   }
 }
 
+function initializeMedia() {
+  if (!('mediaDevices' in navigator)) {
+    navigator.mediaDevices = {};
+  }
+
+  if (!('getUserMedia' in navigator.mediaDevices)) {
+    navigator.mediaDevices.getUserMedia = function (constraints) {
+      const getUserMedia =
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented!'));
+      }
+
+      return new Promise((resolve, reject) => {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    };
+  }
+
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      videoPlayer.srcObject = stream;
+      videoPlayer.style.display = 'block';
+    })
+    .catch((err) => {
+      imagePickerArea.style.display = 'block';
+    });
+}
+
 function openCreatePostModal() {
   createPostArea.style.transform = 'translateY(0)';
+  initializeMedia();
 
   if (window.deferredPrompt) {
     window.deferredPrompt.prompt();
